29b9a21d8f5e49953e59c9861b1aa15552c117b3
Joel <joel.hacke@gmail.com>
2018-02-03 22:05:04 -0600

    added constants file

---
+++ constants.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+import os
+import configparser
+
+"""
+Constants File
+"""
+
+CONFIGFILE = os.path.join(os.path.dirname(__file__), 'config.ini')
+LOGFILE = os.path.join(os.path.dirname(__file__), 'nhl_lights.log')
+SCRIPTPATH = os.path.abspath(__file__)
+
+
+config = configparser.ConfigParser()
+config.read(CONFIGFILE)
+
+BASEURL = "https://statsapi.web.nhl.com/api/v1/"
+NHLBASEURL = "https://statsapi.web.nhl.com/"
+BRIDGEIP = config['hue']['ip']
+HUEUSER = config['hue']['user']
+BRIDGEAPI = 'http://{}/api/{}'.format(BRIDGEIP, HUEUSER)
+CRONUSER = config['user']['user']
+TEAMNAME = config['user']['teamname']
